8e9535b74ac700f19cbaf57c2ca92b439832d814
greg-phillips <greg.phillips@sierratelecom.net>
2024-04-21 16:02:50 -0700

    Added ancs_client.c source

---
+++ src/ble/ancs_client.c
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2021, iMatrix Systems, Inc.. All Rights Reserved.
+ *
+ * This software, associated documentation and materials ("Software"),
+ * is owned by iMatrix Systems ("iMatrix") and is protected by and subject to
+ * worldwide patent protection (United States and foreign),
+ * United States copyright laws and international treaty provisions.
+ * Therefore, you may use this Software only as provided in the license
+ * agreement accompanying the software package from which you
+ * obtained this Software ("EULA").
+ *
+ * Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. iMatrix
+ * reserves the right to make changes to the Software without notice. iMatrix
+ * does not assume any liability arising out of the application or use of the
+ * Software or any product or circuit described in the Software. iMatrix does
+ * not authorize its products for use in any products where a malfunction or
+ * failure of the iMatrix product may reasonably be expected to result in
+ * significant property damage, injury or death ("High Risk Product"). By
+ * including iMatrix's product in a High Risk Product, the manufacturer
+ * of such system or application assumes all risk of such use and in doing
+ * so agrees to indemnify iMatrix against all liability.
+ */
+
+/*
+* @file ancs_client.c
+* @copyright iMatrix Systems, Inc.
+* @date 11/13/2023
+* @author Greg Phillips
+*
+* @brief Describe the purpose of this file
+*
+* A more extensive description that may
+* take multiple lines of text.
+*
+* @version 1.0a
+
+* @bug Untested
+* @todo Nothing
+* @warning No Warning at this time
+
+*/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+
+/******************************************************
+ *                      Macros
+ ******************************************************/
+
+/******************************************************
+ *                    Constants
+ ******************************************************/
+
+/******************************************************
+ *                   Enumerations
+ ******************************************************/
+
+/******************************************************
+ *                 Type Definitions
+ ******************************************************/
+
+/******************************************************
+ *                    Structures
+ ******************************************************/
+
+/******************************************************
+ *               Function Declarations
+ ******************************************************/
+
+/******************************************************
+ *               Variable Definitions
+ ******************************************************/
+
+/******************************************************
+ *               Function Definitions
+ ******************************************************/
+/**
+  * @brief
+  * @param[in]:  None
+  * @param[out]: None
+  * @return:     None
+  */
